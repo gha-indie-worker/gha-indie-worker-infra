c8e641219d13aad2d2d14401ea73d6838a08c273
Alexander Mills <alex@oresoftware.com>
2026-09-15 10:20:18 -0500

    fmt: normalize Neon production root

--- environments/production/neon/main.tf
+++ environments/production/neon/main.tf
@@ -1,8 +1,8 @@
 module "neon_projects" {
   source = "../../../modules/neon/projects"
 
-  neon_org_id                       = var.neon_org_id
-  region_id                         = var.region_id
-  namespace                         = var.namespace
-  admin_private_networking_enabled  = var.admin_private_networking_enabled
+  neon_org_id                      = var.neon_org_id
+  region_id                        = var.region_id
+  namespace                        = var.namespace
+  admin_private_networking_enabled = var.admin_private_networking_enabled
 }
