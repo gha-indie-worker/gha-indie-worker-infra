d72841d804ca98668240fc52e3e0522706458b19
Alexander Mills <alex@oresoftware.com>
2026-09-15 19:00:29 -0500

    DEN-3945 repin rustfmt-clean monorepo aggregate

--- tools/dev-runtime/src/main.rs
+++ tools/dev-runtime/src/main.rs
@@ -9,7 +9,7 @@ use std::{
 };
 
 const MONOREPO_PATH: &str = "_apps/gha-monorepo";
-const EXPECTED_MONOREPO: &str = "608943d2eb0a7f21ce5a0303d426ca853e9dfdab";
+const EXPECTED_MONOREPO: &str = "606b7be87393f5d9590396d3feedd900a461f9c4";
 const ORES_CLI_REV: &str = "c854130ee147e9793a3af8736e90241630a5c934";
 const STUB_API_PIN: &str = "90cfc8a86660d36683fc96d629af843c347e6667";
 const STUB_WEB_PIN: &str = "d99dbb64f3cb4434d023e7f7943a016b7c8c3bd4";
